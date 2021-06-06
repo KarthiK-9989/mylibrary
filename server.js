094e65fd97cc41b8b498edb71e5194da703b82f3
karthik kondpak <karthikkondpak44@gmail.com>
2021-06-06 13:28:50 +0530

    mysecondfile

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-if(process.env.NODE_ENV!=='production'){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load()
 }
 
-const express=require('express');
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
-const app=express();
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
-const expressLayouts=require('express-ejs-layouts');
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(methodOverride('_method'))
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-  const indexRouter=require('./routes/index');
-app.set('view engine','ejs');
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
-app.set('views',__dirname+'/views');
-app.set('layout','layouts/layout');
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
-app.use(expressLayouts);
-app.use(express.static('public'));
-
-const mongoose=require('mongoose');
-
-
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-
-const db=mongoose.connection
-db.on('error',error=>console.error(error))
-db.once('open',()=>console.log('connected to mongoose'))
-app.use('/',indexRouter);
-
-
-app.listen(process.env.PORT|| 3000);
+app.listen(process.env.PORT || 3000)
